46048a0993199cb837c7a6e084ba91c643ca98ec
seb <sebastianjoseph89800@gmail.com>
2021-08-12 15:06:43 +0530

    w

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById('btnSubmit').onclick = function(e){
     name = document.getElementById('inName').value
     document.getElementById('hello').innerText = "Hello " + name    
-  }
+  }hfhfhfhfhf
